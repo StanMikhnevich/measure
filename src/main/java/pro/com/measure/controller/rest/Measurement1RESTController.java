528be712123409c6350393cba83a3044b23e4660
gprohorov <yuramail222@gmail.com>
2020-02-29 20:31:00 +0200

    Api period

--- src/main/java/pro/com/measure/controller/rest/Measurement1RESTController.java
+++ src/main/java/pro/com/measure/controller/rest/Measurement1RESTController.java
@@ -2,8 +2,10 @@ package pro.com.measure.controller.rest;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pro.com.measure.model.Measurement1;
 import pro.com.measure.service.measurement1.impls.Measurement1ServiceImpl;
@@ -30,4 +32,34 @@ public class Measurement1RESTController {
         return service.create(m1);
     }
 
+
+
+    //  returns data for the given hour
+    @RequestMapping(value = "/list/{year}/{month}/{day}/{hour}", method = RequestMethod.GET)
+    List<Measurement1>  showForHour(
+              @PathVariable(value = "year")int year
+            , @PathVariable(value = "month")int month
+            , @PathVariable(value = "day")int day
+            , @PathVariable(value = "hour")int hour
+    ){
+        LocalDateTime start = LocalDateTime.of(year,month,day,hour,0,0);
+        return service.getAllForTheHour(start);
+    }
+
+     // returns data between to dates
+    @RequestMapping(value = "/list/{fromyear}/{frommonth}/{fromday}/{fromhour}/{toyear}/{tomonth}/{today}/{tohour}", method = RequestMethod.GET)
+    List<Measurement1>  showForPeriod(
+            @PathVariable(value = "fromyear")int year
+            , @PathVariable(value = "frommonth")int month
+            , @PathVariable(value = "fromday")int day
+            , @PathVariable(value = "fromhour")int hour
+            , @PathVariable(value = "toyear")int toYear
+            , @PathVariable(value = "tomonth")int toMonth
+            , @PathVariable(value = "today")int toDay
+            , @PathVariable(value = "tohour")int toHour
+    ){
+        LocalDateTime start = LocalDateTime.of(year,month,day,hour,0,0);
+        LocalDateTime finish = LocalDateTime.of(toYear,toMonth,toDay,toHour ,0,0);
+        return service.getAllForThePeriod(start,finish);
+    }
 }
